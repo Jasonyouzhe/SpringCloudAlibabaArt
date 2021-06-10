35996db5d484c8222cedc43e7cd93392686c38f7
zhangdengpan <zhangdengpan@kedacom.com>
2021-06-10 16:52:29 +0800

    springCloudAlibabaBuckets backend

---
+++ spring-cloud-alibaba-buckets-starter/src/main/java/com/spring/cloud/alibaba/buckets/Application.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.alibaba.buckets;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
